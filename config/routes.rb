7eb89e8f1a0e293609edfc6d4cf2ef6600cd3fb7
LilyCole <Lilia.Cole@gmail.com>
2017-03-23 22:14:35 -0700

    updates nav bar and changes status colors

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :dashboards, :except => [:show]
   
+  get 'dashboard', to:'dashboards#index', as: 'dashboard_index'
   get 'feature_requests/', to: 'feature_requests#index', as: 'feature_requests'
   post 'feature_requests/', to: 'feature_requests#new'
 
